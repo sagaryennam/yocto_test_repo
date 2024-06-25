14156b8cfad0321a121b5c0d05bc96dff27df1da
sagaryennam <151729363+sagaryennam@users.noreply.github.com>
2024-06-25 14:01:52 +0530

    example source code

---
+++ example1.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+
+int main()
+{
+	printf("THIS IS FROM EXAMPLE1.c FILE");
+	return 0;
+}
---
+++ example2.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+
+int main()
+{
+	printf("THIS IS FROM example2.c FILE");
+	return 0;
+}
